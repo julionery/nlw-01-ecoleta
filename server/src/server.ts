4c0d40dadaec90a818c853ebb689ca14b70c9eda
Júlio Nery <julio_cesar.an@hotmail.com>
2020-06-03 00:34:54 -0300

    finalizando segundo dia do NLW

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+import path from 'path';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log("Listagem de usuários");
-    response.json(['Júlio', 'Diego', 'Karen', 'Nery']);
-});
+app.use(cors());
+app.use(express.json()); 
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
 
